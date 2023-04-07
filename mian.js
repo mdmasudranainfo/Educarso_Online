02c08c8d04ac540f9263df807a60a6d22334b6fc
mdmasudranainfo <mdmasude7@gmail.com>
2023-04-07 17:16:56 +0600

    couse section

--- mian.js
+++
@@ -1,7 +0,0 @@
-const Collapse = document.getElementById("Collapse");
-
-const menuUL = document.getElementById("menuUL");
-
-Collapse.addEventListener("click", () => {
-  menuUL.classList.toggle("hidden");
-});
