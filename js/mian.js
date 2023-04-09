4c6d1d103aab01ffc09384dab63e0e7fca83ea44
mdmasudranainfo <mdmasude7@gmail.com>
2023-04-09 17:17:45 +0600

    home page done

--- js/mian.js
+++ js/mian.js
@@ -36,6 +36,49 @@ $(".categorySlider").slick({
 });
 // slick slider end
 
+// make  mixitup start
+
+const box1 = $("#box1");
+const box2 = $("#box2");
+const box3 = $("#box3");
+const box4 = $("#box4");
+const box5 = $("#box5");
+const box6 = $("#box6");
+
+function slideUpAll() {
+  box1.slideDown("1000");
+  box2.slideDown("1000");
+  box3.slideDown("1000");
+  box4.slideDown("1000");
+  box5.slideDown("1000");
+  box6.slideDown("1000");
+}
+$(document).ready(function () {
+  $("#btn1").click(function () {
+    slideUpAll();
+  });
+  $("#btn2").click(function () {
+    slideUpAll();
+    box1.slideUp("10000");
+    box3.slideUp("10000");
+    box6.slideUp("10000");
+    box4.slideUp("10000");
+  });
+
+  $("#btn3").click(function () {
+    slideUpAll();
+    box2.slideUp("10000");
+    box5.slideUp("10000");
+  });
+  $("#btn4").click(function () {
+    slideUpAll();
+    box5.slideUp("10000");
+    box1.slideUp("10000");
+    box3.slideUp("10000");
+  });
+});
+// make  mixitup end
+
 //
 //
 //
